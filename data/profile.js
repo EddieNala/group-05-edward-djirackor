a8bfa952c1f2e8cd615cdba174343880b38dd2a5
EddieNala <50713128+EddieNala@users.noreply.github.com>
2024-08-09 16:08:51 +0000

    Add files via upload

---
+++ data/profile.js
@@ -0,0 +1,136 @@
+const profileData = {
+  title: "Resume",
+  name: "Djirackor",
+  sub_title: "Senior Meteorological Technician and Backend Engineer",
+  logoURL: "assets/images/dp.jpg",
+  about: {
+    intro: `Senior Meteorological Technician with 6 years of aviation weather experience. Experience in building databases and REST APIs.`,
+    contact: {
+      email: "djirackoredward@gmail.com",
+      phone: "+233-501373771",
+      address: "Sekondi, Ghana",
+    },
+  },
+  links: [
+    {
+      title: "LinkedIn",
+      src: "https://www.linkedin.com/in/imvpn22",
+      iconClass: "fa-brands fa-linkedin-in",
+    },
+    {
+      title: "Github",
+      src: "https://github.com/imvpn22",
+      iconClass: "fa-brands fa-github",
+    },
+  ],
+  skills: [
+    {
+      title: "Operating System",
+      value: "Linux, Windows",
+    },
+    {
+      title: "Languages",
+      value: "Javascript (Node/Express) & Python",
+    },
+    {
+      title: "Databases",
+      value: "SQL & MongoDB",
+    },
+    {
+      title: "Version Control",
+      value: "Git, GitHub",
+    },
+    {
+      title: "Cloud Computing",
+      value: "AWS",
+    },
+    {
+      title: "Aviation weather",
+      value: "Terminal Aerodrome Forecasts (TAF), METAR, QAM, Synoptic code",
+    },
+  ],
+  experiences: [
+    {
+      organization: "Prempeh II Library",
+      title: "IT Support Officer and Research Assistant",
+      date: "11/2016 - 11/2017",
+      details: [
+        `I made the research for post-graduate students' thesis easier by assisting them in this regard.  I also
+        checked their completed work for plagiarism.`,
+        `I assisted in improving e-resources education by assisting in the organization of research training for
+        students and staff.`, 
+        `I improved general productivity of the e-resources department by providing technical support for staff and users of the
+        facility.`,
+      ],
+    },
+    {
+      organization: "Effia Nkwanta Regional Hospital",
+      title: `System Administrator`,
+      date: "06/2018 - 01/2019",
+      details: [
+        `I kept the hospital running smoothly by offering technical support to all staff.`,
+        `I kept the hospital's network up and repaired broken computers and devices.`,
+        `I trained and re-trained staff.`,
+      ],
+    },
+    {
+      organization: "Ghana Meteorological Agency (Takoradi Airport Station)",
+      title: `Meteorology Technician`,
+      date: "01/2019 - Present",
+      details: [
+        `I work hand in hand with Air Traffic Control to ensure that all
+        aircrafts land safely by providing accurate readings on
+        weather conditions at the airport.`,
+
+        `I contribute to the World Meteorological Organization's
+        goals by encoding weather conditions at my local station and
+        transmitting it in a timely manner.`,
+
+        `I ensure stakeholder satisfaction by sending timely weather
+        updates and addressing their concerns if they have any.`,
+      ],
+    },
+  ],
+  projects: [
+    {
+      title: "Messenger Application",
+      duration: "Oct - Nov 2022",
+      link: "https://github.com/imvpn22/restaurant-review-pwa",
+      desc: `A web application that enables users to text and exchange files in real time. It includes features likes user registration, login and chat rooms.`,
+    },
+    {
+      title: "A Store API",
+      duration: "Jan - Feb 2023",
+      link: "https://github.com/imvpn22/whiteboard",
+      desc: `A well-documented RSET API for clothing Store. It has endpoints for user creation, login and filtering data`,
+    },
+  ],
+  education: [
+    {
+      alma: "Kwame Nkrumah University of Science and Technology",
+      duration: "2012 - 2016",
+      std: " BSc Computer Science",
+      score: "Sixty Something",
+    },
+    {
+      alma: "Amalitech Graduate Training",
+      duration: "2022 - 2023",
+      std: "Backend Engineering",
+      score: "N/A",
+    },
+  ],
+  certifications: [
+    {
+      desc: `<strong>Google IT Support Professional Certificate</strong> by <strong>Coursera</strong>`,
+      date: "May - Nov 2018",
+    },
+  //   {
+  //     desc: `<strong>Introduction to Modern Application Development</strong>, a MOOC by <em> NPTEL (IIT Madras)
+  //     and Hasura</em>. Ranked at <strong> top 10 percentile </strong> among more than 2500 candidates.
+  //      (<a target='_blank' rel='noreferrer' href='https://nptel.ac.in/noc/E_Certificate/linkedin/noc17-cs06/NPTEL17CS0626270067AN.jpg'>
+  //      https://goo.gl/X3HEdR</a>)`,
+  //     date: "Jan - Mar 2017",
+  //   },
+  ],
+  events: [],
+};
